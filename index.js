0b38e61696a6e520374465518fa9fad2c454c040
Craig Sennabaum <csennabaum@gmail.com>
2017-10-20 09:03:57 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const mongoose = require('mongoose')
-const app = express()
 const routes = require('./routes')
+const app = express()
 
 mongoose.Promise = Promise
 
